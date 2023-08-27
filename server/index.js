571513acc86a3ce8defdc50e68d1e7394b352c17
ruo-mungai <ruomungai@gmail.com>
2023-08-27 18:09:13 +0300

    creating mysql connection

--- server/index.js
+++ server/index.js
@@ -3,6 +3,18 @@ import mysql from "mysql";
 
 const app= Express (); // creating a variable to hold express function
 
+//connecting to mysql
+
+const db= mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "2000",
+        database: "test"
+
+    }
+)
+
 app.listen(8800,()=>{
     console.log("connection ok continue")
 })
