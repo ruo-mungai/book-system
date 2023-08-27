1b77a1c1c0ef727bc1e333f1bc31f24f5fcdf933
ruo-mungai <ruomungai@gmail.com>
2023-08-27 19:00:56 +0300

    adding the get books route

--- server/index.js
+++ server/index.js
@@ -19,6 +19,16 @@ app.get ("/",(req,res)=>{
     res.json("CONNECTED TO BACKED END")
 })
 
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '2000'
+app.get ("/books",(req,res)=>{
+    const books="SELECT * FROM books"
+    db.query(books,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+
+    })
+})
+
 app.listen(8800,()=>{
     console.log("connection ok continue")
 })
