050566d979407526ca15025a260fbb14363e51c2
ruo-mungai <ruomungai@gmail.com>
2023-08-27 18:03:31 +0300

    setting up server side

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import Express  from "express";
+import mysql from "mysql";
+
+const app= Express (); // creating a variable to hold express function
+
+app.listen(8800,()=>{
+    console.log("connection ok continue")
+})
